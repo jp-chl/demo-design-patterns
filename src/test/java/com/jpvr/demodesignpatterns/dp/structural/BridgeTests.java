b812730a291af39469d0e1004618040f88693af2
JP <51864825+jp-chl@users.noreply.github.com>
2020-04-24 10:20:41 -0400

    bridge pattern ok

--- src/test/java/com/jpvr/demodesignpatterns/dp/structural/BridgeTests.java
+++ src/test/java/com/jpvr/demodesignpatterns/dp/structural/BridgeTests.java
@@ -85,11 +85,14 @@ public class BridgeTests {
         movie.setRuntime("2:15");
         movie.setYear("2014");
 
-        Formatter printFormatter = new PrintFormatter();
         Printer moviePrinter = new MoviePrinter(movie);
 
+        Formatter printFormatter = new PrintFormatter();
         String printedMaterial = moviePrinter.print(printFormatter);
-
         System.out.println(printedMaterial);
+
+        Formatter htmlFormatter = new HtmlFormatter();
+        String htmlMaterial = moviePrinter.print(htmlFormatter);
+        System.out.println(htmlMaterial);
     } // end
 } // end class BridgeTests
